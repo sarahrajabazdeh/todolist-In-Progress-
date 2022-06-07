bd528b7a416451f47d98deb269dfa65d7edaa36e
sarahrajabzadeh <khode.divergent@gmail.com>
2022-06-07 09:00:16 +0200

    add very first basic layer

---
+++ database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/sarahrajabazdeh/todolist/config"
+)
+
+type Database struct {
+	DB *sql.DB
+}
+
+func CreateDatabase() Database {
+	var err error
+
+	db := Database{}
+
+	// create the connection string
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		config.Config.Database.DbAddr, config.Config.Database.DbPort, config.Config.Database.DbUser, config.Config.Database.DbPass, config.Config.Database.DbName)
+
+	db.DB, err = sql.Open(config.Config.Database.DbType, connectionString)
+	if err != nil {
+		log.Println("error db connection")
+		log.Fatal(err)
+	}
+	// check connection
+	err = db.DB.Ping()
+	if err != nil {
+		log.Println("Cannot connect to the db")
+		log.Fatal(err)
+	}
+	log.Println("Connected to database ", config.Config.Database.DbName)
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sarahrajabazdeh/todolist/config"
+
+	"github.com/sarahrajabazdeh/todolist/database"
+)
 
 func main() {
 	log.Println("Stating Go server")
 
 	// read the configuration
 	config.ReadConfig()
+
+	// create the db connection
+	db := database.CreateDatabase()
+
+	ds := dataservice.Dataservice{
+		DB: &db,
+	}
+
+	ctrl := controller.HttpController{
+		DS: &ds,
+	}
+
+	r := mux.NewRouter()
+	router.SetupRoutes(r, &ctrl)
+
+	err := http.ListenAndServe(":"+config.Config.Server.Port, cors.AllowAll().Handler(r))
+	if err != nil {
+		log.Println(err)
+	}
+
 }
